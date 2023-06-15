5b52643f78dfce803696909aa102a9909af62fad
Arnab-1473 <arban.nilr.1473@gmail.com>
2023-06-15 18:33:12 +0530

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 
 import React from "react";
 import './../styles/App.css';
+import Tooltip from "./Tooltip";
 
 const App = () => {
   return (
     <div>
-        {/* Do not remove the main div */}
+        <Tooltip text={"This is a tooltip"}/>
+        <span className="tooltip">Hover over me</span>
     </div>
   )
 }
